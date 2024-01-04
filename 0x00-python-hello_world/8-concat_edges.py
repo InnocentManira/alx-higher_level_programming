6d3eedc3a4f4fa6950e99351c32ec863eaea9b2e
InnocentManira <innocent.maniragena96@gmail.com>
2024-01-04 11:52:18 -0800

    updates

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
         language that combines remarkable power with very clear syntax"
-str = str[39:67] + str[107:112] + str[:6]
+str = str[9:17] + str[27:32] + str[46:]
 print(str)
